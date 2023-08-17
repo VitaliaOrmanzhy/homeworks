13fe731e0f3b81e06304f175aacfd9e9eb22c692
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-08-17 12:10:50 +0300

    removed adding flats into a building

--- dz20/script.js
+++ dz20/script.js
@@ -1,14 +1,23 @@
 const person1 = new Person('Name1', 'male');
 const person2 = new Person('Name2', 'female');
 const person3 = new Person('Name3', 'male');
+const person4 = new Person('Name4', 'female');
 
 const flat1 = new Flat();
 flat1.addResidents([person1, person2]);
 
 const flat2 = new Flat();
-flat2.addResidents([person3]);
+flat2.addResidents(person3);
 
-const building = new Building(70);
-building.addFlat([flat1, flat2]);
+const flat3 = new Flat();
+flat3.addResidents(person4);
 
+
+const building = new Building(4);
+
+building.addFlat(flat1);
+building.addFlat(flat1);
+building.addFlat(flat1);
+building.addFlat(flat1);
+building.addFlat(flat1);
 console.log(building);
