57979e6874faa323e9b11ee9bd0271c00bbe4fdc
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-08-19 21:32:40 +0300

    removed methods from Hamburger class

--- dz21_version2/Hamburger.js
+++ dz21_version2/Hamburger.js
@@ -1,45 +1,12 @@
-class Hamburger {
+class Hamburger extends Info {
     constructor(size, filling) {
+        super();
         this.properties = {
             'size': size,
             'filling': filling,
         };
     }
 
-    static SIZE_SMALL = {
-        calories: 20,
-        price: 50     
-    }
-
-    static SIZE_BIG = {
-        calories: 40,
-        price: 100  
-    }
-
-    static STUFFING_CHEESE = {
-        calories: 20,
-        price: 10
-    }
-
-    static SALAD = {
-        calories: 5,
-        price: 20
-    }
-
-    static POTATO = {
-        calories: 10,
-        price: 15
-    }
-
-    static TOPPING_SAUCE = {
-        calories: 0,
-        price: 15
-    }
-
-    static TOPPING_MAYO = {
-        calories: 15,
-        price: 20
-    }
 
 
     addTopping(topping) {
