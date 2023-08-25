1c6f089edcad6c11566b0ff12d815200c9bdc07d
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-08-25 13:54:28 +0300

    2 examples of students

---
+++ dz22/script.js
@@ -0,0 +1,62 @@
+const student1 = new Student('Vita', 'Ormanzhy', 2007, [90, 100, 90, 80, 100]);
+console.log('Student1');
+console.log('age:', student1.getAge());
+console.log('GPA:', student1.getGPA());
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.absent();
+
+console.log(student1.attendance);
+console.log(student1.summary())
+
+const student2 = new Student('Name', 'Surname', 2005, [100, 100, 100, 80]);
+console.log('Student2');
+console.log('age:', student2.getAge());
+console.log('GPA:', student2.getGPA());
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+
+console.log(student2.attendance);
+console.log(student2.summary())
