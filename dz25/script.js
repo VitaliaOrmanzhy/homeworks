c91ce480c40cf74b784bc65ff2544dae4f0efb2d
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-08-29 17:29:04 +0300

    addImages func

--- dz25/script.js
+++ dz25/script.js
@@ -1,1 +1,9 @@
 const sliderImagesContainer = document.querySelector('.slider__images');
+
+function addImages() {
+    for (let i = 1; i <= 6; i++) {
+        sliderImagesContainer.innerHTML += `<img src="images/${i}.jpg"/>`;
+    }  
+}
+
+addImages()
