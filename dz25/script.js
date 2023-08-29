d54ac6d120b86cc8ad8efa7ffd883dbf17186cac
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-08-29 17:30:37 +0300

    changed name of addImages to renderImages

--- dz25/script.js
+++ dz25/script.js
@@ -1,9 +1,9 @@
 const sliderImagesContainer = document.querySelector('.slider__images');
 
-function addImages() {
+function renderImages() {
     for (let i = 1; i <= 6; i++) {
         sliderImagesContainer.innerHTML += `<img src="images/${i}.jpg"/>`;
     }  
 }
 
-addImages()
+renderImages()
