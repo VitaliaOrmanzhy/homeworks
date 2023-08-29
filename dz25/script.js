0eab208ea5852c26c59fe91ee22e3f28b5413b56
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-08-29 22:33:01 +0300

    nextFunc, prevFunc

--- dz25/script.js
+++ dz25/script.js
@@ -12,3 +12,46 @@ function renderImages() {
 }
 
 renderImages();
+
+let px = 0;
+const nextBtn = document.querySelector('.slider__next');
+const prevBtn = document.querySelector('.slider__prev');
+
+function nextFunc() {
+    //prevBtn з'являється
+    prevBtn.style.display = 'block';
+
+    //к-сть картинок
+    const imagesLength = document.querySelectorAll('.slider__image').length;
+
+    if (px != (imagesLength - 1) * 800 - 800) {
+        sliderImagesContainer.style.right = `${px + 800}px`;
+        px += 800;
+    }
+    
+    if (px == (imagesLength - 1) * 800 - 800) {
+        nextBtn.style.display = 'none';
+    }
+    // if (px == ((document.querySelectorAll('.slider__image').length - 1) * 800) - 800)
+        
+}
+
+function prevFunc() {
+    //nextBtn з'являєьться
+    nextBtn.style.display = 'block';
+
+    //к-сть картинок
+    const imagesLength = document.querySelectorAll('.slider__image').length;
+
+    if (px != 0) {
+        sliderImagesContainer.style.right = `${px - 800}px`;
+        px -= 800;
+    }
+    
+    if (px == 0) {
+        prevBtn.style.display = 'none';
+    }
+}
+
+nextBtn.addEventListener('click', nextFunc);
+prevBtn.addEventListener('click', prevFunc);
