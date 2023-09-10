9df80d52dbfa9038782918baf01f06b8d80b86ba
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-09-10 13:54:28 +0300

    added product-info title

--- dz28/ProductInfo.js
+++ dz28/ProductInfo.js
@@ -2,9 +2,10 @@ class ProductInfo {
 
     render(idOfProduct) {
         const product = catalog.find(item => item.id == idOfProduct);
+        const productName = product.name;
         const productInfo = product.info;
 
-        let info = '';
+        let info = `<h2 class="product-info__title">${productName}</h2>`;
         productInfo.map(item => {
             info += `<p class="product-info__paragraph">${item}</p>`
         })
